7f4b4b351b9880c381c44824fe2c3870a6d1ff91
UP776193 <33348872+UP776193@users.noreply.github.com>
2019-11-18 15:30:11 +0000

    Added more blocks and reworked slots

--- ProjectApplication/app/src/main/java/com/example/landscapedesign/ProblemActivity.java
+++ ProjectApplication/app/src/main/java/com/example/landscapedesign/ProblemActivity.java
@@ -5,47 +5,113 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import static java.security.AccessController.getContext;
-
 public class ProblemActivity extends AppCompatActivity {
 
-    TextView block1, block2, block3, block4, target;
+    TextView block1, block2, block3, block4, block5, block6, block7, block8, slot1, slot2, slot3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
-        target = findViewById(R.id.tvProblemWindow);
-        String problem =
-                "public void addTo25() {\n" +
-                "\t\t\tint x = 0;\n" +
-                "\t\t\tfor (int i = 0; i<4; i++) {\n" +
-                "\t\t\t\t\t\tx += 5;\n" +
-                "\t\t\t}\n" +
-                "\t\t\treturn x;\n" +
-                "}";
-        target.setText(problem);
 
         block1 = findViewById(R.id.block1);
         block2 = findViewById(R.id.block2);
         block3 = findViewById(R.id.block3);
         block4 = findViewById(R.id.block4);
+        block5 = findViewById(R.id.block5);
+        block6 = findViewById(R.id.block6);
+        block7 = findViewById(R.id.block7);
+        block8 = findViewById(R.id.block8);
 
-        block1.setOnLongClickListener(longclickListener);
-        block2.setOnLongClickListener(longclickListener);
-        block3.setOnLongClickListener(longclickListener);
-        block4.setOnLongClickListener(longclickListener);
+        slot1 = findViewById(R.id.slot1);
+        slot2 = findViewById(R.id.slot2);
+        slot3 = findViewById(R.id.slot3);
 
-        target.setOnDragListener(dragListenre);
+        //block1.setOnLongClickListener(longclickListener);
+        //block2.setOnLongClickListener(longclickListener);
+        //block3.setOnLongClickListener(longclickListener);
+        //block4.setOnLongClickListener(longclickListener);
+        //block5.setOnLongClickListener(longclickListener);
+        //block6.setOnLongClickListener(longclickListener);
+        //block7.setOnLongClickListener(longclickListener);
+        //block8.setOnLongClickListener(longclickListener);
+
+        block1.setOnTouchListener(new BlockTouchListener());
+        block2.setOnTouchListener(new BlockTouchListener());
+        block3.setOnTouchListener(new BlockTouchListener());
+        block4.setOnTouchListener(new BlockTouchListener());
+        block5.setOnTouchListener(new BlockTouchListener());
+        block6.setOnTouchListener(new BlockTouchListener());
+        block7.setOnTouchListener(new BlockTouchListener());
+        block8.setOnTouchListener(new BlockTouchListener());
+
+        slot1.setOnDragListener(new SlotDragListener());
+        slot2.setOnDragListener(new SlotDragListener());
+        slot3.setOnDragListener(new SlotDragListener());
+
+        //target.setOnDragListener(dragListenre);
     }
+
+    private final class BlockTouchListener implements View.OnTouchListener {
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDragAndDrop(data, shadowBuilder, view, 0);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    private class SlotDragListener implements View.OnDragListener {
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    //handle the dragged view being dropped over a drop view
+                    View view = (View) event.getLocalState();
+                    view.setVisibility(View.INVISIBLE);
+                    TextView dropTarget = (TextView) v;
+                    TextView dropped = (TextView) view;
+                    dropTarget.setText(dropped.getText());
+                    Object tag = dropTarget.getTag();
+                    if(tag != null) {
+                        int existingID = (Integer)tag;
+                        findViewById(existingID).setVisibility(View.VISIBLE);
+                    }
+                    dropTarget.setTag(dropped.getId());
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+
 
     View.OnLongClickListener longclickListener = new View.OnLongClickListener() {
         @RequiresApi(api = Build.VERSION_CODES.N)
@@ -58,6 +124,7 @@ public class ProblemActivity extends AppCompatActivity {
         }
     };
 
+    /*
     View.OnDragListener dragListenre = new View.OnDragListener(){
 
         @Override
@@ -89,6 +156,8 @@ public class ProblemActivity extends AppCompatActivity {
         }
     };
 
+     */
+
     public void skip(View view) {
         //found @https://stackoverflow.com/questions/2478517/how-to-display-a-yes-no-dialog-box-on-android
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -100,7 +169,7 @@ public class ProblemActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 // Do nothing but close the dialog
                 String message = "You have skipped me!";
-                TextView problemText = findViewById(R.id.tvProblemWindow);
+                TextView problemText = findViewById(R.id.slot1);
                 problemText.setText(message);
                 dialog.dismiss();
             }
@@ -110,7 +179,7 @@ public class ProblemActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String message = "You have NOT skipped me!";
-                TextView problemText = findViewById(R.id.tvProblemWindow);
+                TextView problemText = findViewById(R.id.slot1);
                 problemText.setText(message);
                 dialog.dismiss();
             }
@@ -162,7 +231,7 @@ public class ProblemActivity extends AppCompatActivity {
     public void submit(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        if(target.getText() == "Block 1 has been dropped here") {
+        if(checkCorrect()) {
             builder.setTitle("CORRECT");
             builder.setMessage("Block 1 is the correct answer");
             builder.setNeutralButton("Next Question", new DialogInterface.OnClickListener() {
@@ -183,5 +252,9 @@ public class ProblemActivity extends AppCompatActivity {
         }
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    public boolean checkCorrect() {
+        return true;
     }
 }
